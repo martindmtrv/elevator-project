6c594b30064a4fd0f62869fa6b53cf804874a6ef
Martin Dimitrov <martin3dimitrov@gmail.com>
2021-02-02 16:13:57 -0500

    removed floor threads and added configuration file for convenience

--- src/Floor.java
+++ src/Floor.java
@@ -4,9 +4,7 @@
  * @author Martin Dimitrov
  *
  */
-public class Floor implements Runnable {
-	static final int MAX_FLOOR = 10, MIN_FLOOR = 0;
-	
+public class Floor {	
 	private int floorNum; /* The number of the floor */
 	
 	// floor buttons
@@ -17,33 +15,27 @@ public class Floor implements Runnable {
 	private FloorLamp upLamp;
 	private FloorLamp downLamp;
 	
-	// buffer for this floors inputs (maybe will need a buffer for outputs?)
-	private BoundedBuffer buffer;
-	
-	
 	/**
 	 * Create a new floor object
 	 * @param f - floor number
-	 * @param b - bounded buffer for this floor
 	 */
-	Floor(int f, BoundedBuffer b) {
+	Floor(int f) {
 		// maybe should check here max sure it's in range
 		floorNum = f;
-		buffer = b;
 		
 		// floor will either have both directions or just one (top and bottom floors)
-		if (floorNum != MAX_FLOOR) {
+		if (floorNum != Configuration.NUM_FLOORS) {
 			upButton = new FloorButton(FloorButton.UP);
 			upLamp = new FloorLamp(FloorButton.UP);
 		}
-		if (floorNum != MIN_FLOOR) {
+		if (floorNum != 1) {
 			downButton = new FloorButton(FloorButton.DOWN);
 			downLamp = new FloorLamp(FloorButton.DOWN);
 		}
 	}
 	
 	/**
-	 * What happens when a request to go up is sent (this is like user clicking up button)
+	 * Request to go up is sent (this is like user clicking up button)
 	 * @return status (success or not)
 	 */
 	public boolean requestUp() {
@@ -57,7 +49,7 @@ public class Floor implements Runnable {
 	}
 	
 	/**
-	 * Request to go down is sent to the floor subsystem, (this is like user clicking down button)
+	 * Request to go down (this is like user clicking down button)
 	 * @return status (success or not)
 	 */
 	public boolean requestDown() {
@@ -68,26 +60,5 @@ public class Floor implements Runnable {
 			return true;
 		}
 		return false;
-	}
-	
-	@Override
-	public void run() {
-		String input;
-		String[] split;
-		
-		while (!Thread.interrupted()) {
-			input = (String) buffer.removeFirst();
-			split = input.split(" ");
-			
-			// TODO: change with the data structure after
-			if (split[2].equals("Down")) {
-				requestDown();
-			} else {
-				requestUp();
-			}
-			
-			System.out.println(String.format("FLOOR %d: Received Input %s", floorNum, input));
-		}
-		
 	}
 }
