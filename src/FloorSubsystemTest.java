43f96440f08b8176d2d658b09012467b646a0087
Martin Dimitrov <martin3dimitrov@gmail.com>
2021-02-05 00:15:21 -0500

    fix tests

--- src/FloorSubsystemTest.java
+++ src/FloorSubsystemTest.java
@@ -1,25 +1,20 @@
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.util.ArrayList;
 
-class FloorSubsystemTest {
+import org.junit.jupiter.api.Test;
+
+public class FloorSubsystemTest {
 
     private FloorSubsystem floorSubsystem;
     //Test data in input Test.txt file
     private final String[] inputData = {"14:05:15.0 2 Up 4","14:15:30.0 3 Down 1","15:30:10.5 7 Down 2","14:05:15.0 2 Up 4","14:15:30.0 3 Down 1","15:30:10.5 7 Down 2","14:05:15.0 2 Up 4","14:15:30.0 3 Down 1","15:30:10.5 7 Down 2"} ;
-    private final String testFile = "Test.txt";
-
-    @Before
-    void setUp() {
-        floorSubsystem = new FloorSubsystem(testFile,10);
-    }
+    private final String testFile = "Test.txt";    
 
     @Test
     public void testCreateFloorSubsystem(){
+    	
         assertNotEquals(floorSubsystem,null);
     }
 
@@ -32,5 +27,11 @@ class FloorSubsystemTest {
         for(int i=0;i<fileInput.size();i++){
             assertEquals(fileInput.get(i),inputData[i]);
         }
+    }
+    
+    @Test
+    public void test() {
+    	System.out.println("testing");
+    	assertEquals(true, true);
     }
 }
